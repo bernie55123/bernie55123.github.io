238e841b2259a43cee38c0dc7248521019969a5f
bo wei chou <bernie3305730@gmail.com>
2023-08-16 22:02:47 +0800

    新增交易請求

---
+++ static/js/new_trade_request.js
@@ -0,0 +1,35 @@
+const doneBtn = document.getElementById('doneBtn');
+doneBtn.addEventListener("click", function () {
+  var form = new FormData();
+  var email = getLocalStorage("email");
+  var balance = getLocalStorage("balance");
+  var name = document.getElementById("task_name").value; // "test001";
+  var overview = document.getElementById("overview").value;// "overview123";
+  var token = document.getElementById("token").value; // "test001";
+  var cover = getLocalStorage("task_cover");//`${TASK_COVER}`;
+
+  
+  form.append("uuid", uuid);
+  form.append("email", email);
+  form.append("balance", balance);
+  form.append("type", type);
+  form.append("name", name);
+  form.append("token", token);
+  form.append("overview", overview);
+  form.append("cover", cover);
+
+  let settings = {
+    "url": `http://127.0.0.1:8000/`,
+    "method": "POST",
+    "timeout": 0,
+    "processData": false,
+    "mimeType": "multipart/form-data",
+    "contentType": false,
+    "data": form
+  };
+
+  $.ajax(settings).done(function (res) {
+    console.log(res);
+    //window.location.replace("/verifier-cms-list.html");
+  });
+});
